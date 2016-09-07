65619542d9751db2b0ddc9e728fefae063f1d3cc
David Luecke <daff@neyeon.com>
2016-09-06 22:02:27 -0700

    Add updated tests and functionality

--- test/index.test.js
+++ test/index.test.js
@@ -7,73 +7,63 @@ import service from '../src';
 import server from './test-app';
 
 describe('Feathers MongoDB Service', () => {
-  const _ids = {};
-  const app = feathers()
-    .use('/people', service({ Model: {} }));
+  const app = feathers();
 
   let db;
 
   before(() =>
-    MongoClient.connect('mongodb://localhost:27017/feathers-test').then(function(database) {
-      db = database;
-      app.service('people').Model = db.collection('people');
+    MongoClient.connect('mongodb://localhost:27017/feathers-test')
+      .then(function(database) {
+        db = database;
 
-      db.collection('people').removeMany();
-      db.collection('todos').removeMany();
-    })
+        app.use('/people', service({
+            Model: db.collection('people'),
+            events: [ 'testing' ]
+          })).use('/people-customid', service({
+            Model: db.collection('people-customid'),
+            id: 'customid',
+            events: [ 'testing' ]
+          }));
+
+        db.collection('people-customid').removeMany();
+        db.collection('people').removeMany();
+        db.collection('todos').removeMany();
+      })
   );
 
   after(() => db.dropDatabase().then(() => db.close()));
 
-  it('is CommonJS compatible', () => {
-    expect(typeof require('../lib')).to.equal('function');
-  });
+  it('is CommonJS compatible', () =>
+    expect(typeof require('../lib')).to.equal('function')
+  );
 
+  base(app, errors, 'people', '_id');
+  base(app, errors, 'people-customid', 'customid');
 
   describe('Initialization', () => {
     describe('when missing options', () => {
-      it('throws an error', () => {
-        expect(service.bind(null)).to.throw('MongoDB options have to be provided');
-      });
+      it('throws an error', () =>
+        expect(service.bind(null)).to.throw('MongoDB options have to be provided')
+      );
     });
 
     describe('when missing a Model', () => {
-      it('throws an error', () => {
-        expect(service.bind(null, {})).to.throw('MongoDB collection `Model` needs to be provided');
-      });
+      it('throws an error', () =>
+        expect(service.bind(null, {})).to.throw('MongoDB collection `Model` needs to be provided')
+      );
     });
 
     describe('when missing the id option', () => {
-      it('sets the default to be _id', () => {
-        expect(service({ Model: db }).id).to.equal('_id');
-      });
+      it('sets the default to be _id', () =>
+        expect(service({ Model: db }).id).to.equal('_id')
+      );
     });
 
     describe('when missing the paginate option', () => {
-      it('sets the default to be {}', () => {
-        expect(service({ Model: db }).paginate).to.deep.equal({});
-      });
+      it('sets the default to be {}', () =>
+        expect(service({ Model: db }).paginate).to.deep.equal({})
+      );
     });
-  });
-
-  describe('Common functionality', () => {
-    beforeEach(function(done) {
-      db.collection('people').insert({
-        name: 'Doug',
-        age: 32
-      }, function(error, data) {
-        if(error) {
-          return done(error);
-        }
-
-        _ids.Doug = data.insertedIds[0];
-        done();
-      });
-    });
-
-    afterEach(() => db.collection('people').remove({ _id: _ids.Doug }));
-
-    base(app.service('people'), _ids, errors, '_id');
   });
 
   describe('MongoDB service example test', () => {
